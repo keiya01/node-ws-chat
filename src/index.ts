2d484240b92e18a5a6c8fabd5e600f6f10448256
keiya01 <keiya.s.0210@gmail.com>
2019-12-29 13:58:57 +0900

    Fix wss

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,62 @@
-import { Server } from "ws";
+import { Server, OPEN } from "ws";
 import http from "http";
 import redis from "redis";
+import net from "net";
+
+const server = http.createServer();
+const wss = new Server({ noServer: true });
 
 const port = process.argv[2] || 9000;
 
-const setup = () => {
-  const server = http.createServer();
-  const wss = new Server({ server });
-  
-  return { server, wss };
-}
-
-const broadcast = (wss: Server) => {
+const broadcast = () => {
   const redisSub = redis.createClient();
   redisSub.subscribe("chat_messages");
-  redisSub.on("message", (_, message) => {
+  redisSub.on("message", (_, json) => {
+    const data = JSON.parse(json);
     wss.clients.forEach(client => {
-      client.send(message);
+      if(client.readyState === OPEN) {
+        client.send(data.message);
+      }
     });
   });
 }
 
-const connect = () => {
-  const redisPub = redis.createClient();
-  const { server, wss } = setup();
+const originWhiteList = [
+  "http://localhost:3000",
+  "http://localhost:3001"
+];
 
-  
+const handleUpgradeWSS = () => {
+  server.on("upgrade", (request: http.IncomingMessage, socket: net.Socket, head: Buffer) => {
+    const origin = request.headers["access-control-allow-origin"];
+    if(origin && !originWhiteList.includes(origin)) {
+      return socket.destroy();
+    }
+
+    wss.handleUpgrade(request, socket, head, ws => {
+      wss.emit("connection", ws, request);
+    });
+  });
+}
+
+const handleConnectWSS = () => {
+  const redisPub = redis.createClient();
   wss.on("connection", ws => {
     console.log("connected!!");
     ws.on("message", msg => {
       console.log(`Listenning this port ${port}`);
       console.log(`Recieved Message: ${msg}`);
-      redisPub.publish("chat_messages", msg.toString());
+      redisPub.publish("chat_messages", JSON.stringify({
+        message: msg.toString()
+      }));
     });
   });
+}
 
-  broadcast(wss);
+const connect = () => {
+  handleConnectWSS();
+  handleUpgradeWSS();
+  broadcast();
   
   server.listen(port);
 }
